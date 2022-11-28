3896c6b2c752eb8a4fd37360a02ea471e240d7f7
Jacob Rein <jakerein@gmail.com>
2022-11-28 10:56:43 -0500

    trying to get things published

--- patterninput/build.gradle.kts
+++ patterninput/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
+    id("maven-publish")
 }
 
 android {
@@ -38,6 +39,19 @@ android {
     publishing {
         singleVariant("release") {
             withSourcesJar()
+        }
+    }
+}
+
+publishing {
+    publications {
+        // Creates a Maven publication called "release".
+        register<MavenPublication>("release") {
+            // You can then customize attributes of the publication as shown below.
+            groupId = "com.github.jakepurple13"
+            artifactId = "final"
+            version = "1.0.0"
+            afterEvaluate { from(components["release"]) }
         }
     }
 }
