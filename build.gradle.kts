e950edf1f757993562456e164a4f483e138af01c
Jacob Rein <jakerein@gmail.com>
2022-11-29 09:10:15 -0500

    MASSIVE cleanup to centralize the main repeated gradle code

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,3 +4,83 @@ plugins {
     id("com.android.library") version "7.3.0" apply false
     kotlin("android") version libs.versions.kotlin.get() apply false
 }
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+
+    dependencies {
+        classpath(libs.androidGradle)
+        classpath(libs.kotlinGradle)
+    }
+}
+
+subprojects {
+    afterEvaluate {
+        configureKotlinCompile()
+        configureAndroidBasePlugin()
+    }
+}
+
+fun Project.configureKotlinCompile() {
+    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+        kotlinOptions {
+            jvmTarget = "1.8"
+        }
+    }
+}
+
+fun Project.configureAndroidBasePlugin() {
+    extensions.findByType<com.android.build.gradle.BaseExtension>()?.apply {
+        compileSdkVersion(33)
+
+        defaultConfig {
+            minSdk = 23
+            targetSdk = 33
+
+            testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        }
+
+        compileOptions {
+            isCoreLibraryDesugaringEnabled = true
+            sourceCompatibility = JavaVersion.VERSION_1_8
+            targetCompatibility = JavaVersion.VERSION_1_8
+        }
+        composeOptions {
+            kotlinCompilerExtensionVersion = libs.versions.compose.compiler.get()
+        }
+        packagingOptions {
+            resources {
+                excludes += "/META-INF/{AL2.0,LGPL2.1}:"
+            }
+        }
+
+        buildTypes {
+            getByName("release") {
+                isMinifyEnabled = false
+                proguardFiles(
+                    getDefaultProguardFile("proguard-android-optimize.txt"),
+                    "proguard-rules.pro",
+                )
+            }
+        }
+
+        buildFeatures.compose = true
+        dependencies {
+            val coreLibraryDesugaring by configurations
+            coreLibraryDesugaring(libs.coreLibraryDesugaring)
+            "implementation"(libs.androidCore)
+            "implementation"(libs.bundles.compose)
+            "testImplementation"("junit:junit:4.13.2")
+            "androidTestImplementation"("androidx.test.ext:junit:1.1.4")
+            "androidTestImplementation"("androidx.test.espresso:espresso-core:3.5.0")
+        }
+    }
+}
+
+fun Project.configurePublishing() {
+
+}
--- groupbutton/build.gradle.kts
+++ groupbutton/build.gradle.kts
@@ -6,36 +6,9 @@ plugins {
 
 android {
     namespace = "com.programmersbox.groupbutton"
-    compileSdk = 33
-
     defaultConfig {
-        minSdk = 23
-        targetSdk = 33
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
     }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
-    buildFeatures {
-        compose = true
-    }
-    composeOptions {
-        kotlinCompilerExtensionVersion = libs.versions.compose.compiler.get()
-    }
-
     publishing {
         singleVariant("release") {
             withSourcesJar()
@@ -57,9 +30,5 @@ publishing {
 }
 
 dependencies {
-    implementation(libs.androidCore)
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.4")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.0")
-    implementation(libs.bundles.compose)
+
 }
