b4425801d27051bad53593bbfe92ed0305aaca7a
Jacob Rein <jakerein@gmail.com>
2022-11-29 15:13:32 -0500

    libs update

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "7.3.0" apply false
-    id("com.android.library") version "7.3.0" apply false
+    id("com.android.application") version libs.versions.androidGradle.get() apply false
+    id("com.android.library") version libs.versions.androidGradle.get() apply false
     kotlin("android") version libs.versions.kotlin.get() apply false
 }
 
